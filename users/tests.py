8995a19538ec57c30e1e55ecae5fabb940b7a4d1
Rohit Mahajan <tfrohit02@gmail.com>
2026-09-21 22:06:30 +0530

    Update users/tests.py

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Stock
+
+
+class StockModelTests(TestCase):
+	def test_string_representation_handles_nullable_item_name(self):
+		stock = Stock.objects.create(category='Packaging', item_name=None, quantity=None)
+
+		self.assertEqual(str(stock), 'Unnamed item 0')
