c97597aac4b8cb98334dcdbe839d858404899000
Rohit Mahajan <tfrohit02@gmail.com>
2026-09-21 22:06:29 +0530

    Update couriermanage/views.py

--- couriermanage/views.py
+++ couriermanage/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import ListView
-from django.db.models import Q
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from .models import Courier
 
@@ -11,17 +8,17 @@ def home(request):
 
 @login_required
 def main(request):
-	couriers = Courier.objects.all()
+	couriers = Courier.objects.all().order_by('-id')
 	query = request.GET.get('q')
 	if query:
-		couriers = Courier.objects.filter(
-			Q(student_rollno__icontains=query) | Q(date_recieved__icontains=query) |
-			Q(service__icontains=query)
-			)
+		couriers = Courier.objects.filter(package_no__icontains=query).order_by('-id')
 	paginator = Paginator(couriers, 10)
 	page_number = request.GET.get('page')
 	page_obj = paginator.get_page(page_number)
-	return render(request, 'couriermanage/main.html', {'page_obj': page_obj})
+	return render(request, 'couriermanage/main.html', {'page_obj': page_obj, 'query': query or ''})
 
 def about(request):
 	return render(request, 'couriermanage/about.html', {'title': 'About'})
+
+def upcoming(request):
+	return render(request, 'couriermanage/upcoming.html', {'title': 'Coming soon'})
